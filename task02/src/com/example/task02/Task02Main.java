e32cbd323669df94029057305bd655d84c146925
Dell <Dell@DESKTOP-JRO69R2>
2023-12-15 15:52:47 +0300

    new

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,21 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n > 0 && n < 17)
+        {
+            int length = (int) Math.pow(2, n);
+            return IntStream
+                    .iterate(0, i -> i + 1)
+                    .map(x -> (x % length) ^ ((x % length) >> 1));
 
-        return null; // your implementation here
-
+        }
+        throw new IllegalArgumentException();
     }
 
 }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,7 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -20,8 +21,18 @@ public class Task03Main {
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
-            BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-        // your implementation here
+            BiConsumer<? super T, ? super T> minMaxConsumer)
+    {
+        Iterator<? extends T> iterator = stream.iterator();
+        T current = iterator.hasNext() ? iterator.next() : null;
+        T min = current;
+        T max = current;
+        while (iterator.hasNext())
+        {
+            current = iterator.next();
+            if (order.compare(current, min) < 0) min = current;
+            else if (order.compare(current, max) > 0) max = current;
+        }
+        minMaxConsumer.accept(min, max);
     }
 }
