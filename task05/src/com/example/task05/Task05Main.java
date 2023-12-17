e3061f331d654bb6cd29847db0c30d9d9e75ffad
Dell <Dell@DESKTOP-JRO69R2>
2023-12-17 12:20:34 +0300

    new

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,27 +1,21 @@
 package com.example.task05;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Task05Main {
 
     public static void main(String[] args) {
 
-        /*
-
-        // Random variables
-        String randomFrom = "..."; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
-        String randomTo = "...";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        Random variables;
+        String randomFrom = ".3"; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        String randomTo = "10.";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
         int randomSalary = 100;  // Некоторое случайное целое положительное число. Можете выбрать его самостоятельно.
 
         // Создание списка из трех почтовых сообщений.
         MailMessage firstMessage = new MailMessage(
                 "Robert Howard",
                 "H.P. Lovecraft",
-                "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
-        );
+                "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!");
 
         assert firstMessage.getFrom().equals("Robert Howard") : "Wrong firstMessage from address";
         assert firstMessage.getTo().equals("H.P. Lovecraft") : "Wrong firstMessage to address";
@@ -43,7 +37,7 @@ public class Task05Main {
                 firstMessage, secondMessage, thirdMessage
         );
 
-        // Создание почтового сервиса.
+
         MailService<String> mailService = new MailService<>();
 
         // Обработка списка писем почтовым сервисом
@@ -88,7 +82,7 @@ public class Task05Main {
         assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
 
 
-        */
+
 
     }
 
